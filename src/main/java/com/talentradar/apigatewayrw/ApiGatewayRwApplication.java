db02c182b0094fc4dd14261f1f00fa184886f4b8
Buntu Levy <buntulevycaleb@gmail.com>
2025-07-15 14:36:15 +0200

    fix:the security of .env.example

---
+++ src/main/java/com/talentradar/apigatewayrw/ApiGatewayRwApplication.java
@@ -0,0 +1,15 @@
+package com.talentradar.apigatewayrw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayRwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayRwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talentradar/apigatewayrw/ApiGatewayRwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentradar.apigatewayrw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayRwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
